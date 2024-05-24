386674c2df299aa5a09438e92e845a1200f8cf24
arysannjayaa <wayanarisanjaya01@gmail.com>
2024-05-24 22:54:00 +0800

    feature criteria

--- application/views/template_auth/footer_auth.php
+++ application/views/template_auth/footer_auth.php
@@ -1,10 +1,10 @@
 	<!-- ============================================================== -->
 	<!-- All Required js -->
 	<!-- ============================================================== -->
-	<script src="<?= BASEURL ?>assets/libs/jquery/dist/jquery.min.js"></script>
+	<script src="<?= base_url('') ?>assets/libs/jquery/dist/jquery.min.js"></script>
 	<!-- Bootstrap tether Core JavaScript -->
-	<script src="<?= BASEURL ?>assets/libs/popper.js/dist/umd/popper.min.js"></script>
-	<script src="<?= BASEURL ?>assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="<?= base_url('') ?>assets/libs/popper.js/dist/umd/popper.min.js"></script>
+	<script src="<?= base_url('') ?>assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
 	<!-- ============================================================== -->
 	<!-- This page plugin js -->
 	<!-- ============================================================== -->
