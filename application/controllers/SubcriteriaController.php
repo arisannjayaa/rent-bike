e65bdd47cc729a5af60fa627793994c6ecaaa79e
DESKTOP-8NSJ87U\Ari Sanjaya <wayanarisanjaya01@gmail.com>
2024-05-25 16:32:29 +0800

    refactor: feature subcriteria

--- application/controllers/SubcriteriaController.php
+++ application/controllers/SubcriteriaController.php
@@ -4,22 +4,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class SubcriteriaController extends CI_Controller
 {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	public function __construct()
 	{
 		parent::__construct();
@@ -30,104 +14,163 @@ class SubcriteriaController extends CI_Controller
 		}
 	}
 
+	/**
+	 * load view
+	 * @return string
+	 */
 	public function index()
 	{
 		$data['subkriteria'] = $this->Subcriteria->get_data('subkriteria')->result();
 		$data['kriteria'] = $this->Criteria->get_data('kriteria')->result();
-		$data['title'] = 'Data Kriteria';
-		$data['user'] = $this->db->get_where('user', ['email' =>
+		$data['user'] = $this->db->get_where('user', [
+			'email' => $this->session->userdata('email')
+		])->row_array();
 
-		$this->session->userdata('email')])->row_array();
-
-		if ($this->form_validation->run() == false) {
-			$this->load->view('template/header', $data);
-			$this->load->view('template/navbar', $data);
-			$this->load->view('admin/subcriteria/index', $data);
-			$this->load->view('template/footer');
-		}
+		return view('admin/subcriteria/index', $data);
 	}
 
-	public function create()
+	/**
+	 * load table
+	 * @return void
+	 */
+	public function table()
 	{
-		$data['subkriteria'] = $this->Subcriteria->get_data('subkriteria')->result();
-		$data['kriteria'] = $this->Criteria->get_data('kriteria')->result();
-		$data['title'] = 'Data Subkriteria';
-		$data['user'] = $this->db->get_where('user', ['email' =>
-
-		$this->session->userdata('email')])->row_array();
-
-		if ($this->form_validation->run() == false) {
-			$this->load->view('template/header', $data);
-			$this->load->view('template/navbar', $data);
-			$this->load->view('admin/subcriteria/index', $data);
-			$this->load->view('template/footer');
+		if (!$this->input->is_ajax_request()) {
+			exit('No direct script access allowed');
 		}
+
+		$param['draw'] = isset($_REQUEST['draw']) ? $_REQUEST['draw'] : '';
+		$start = isset($_REQUEST['start']) ? $_REQUEST['start'] : '';
+		$length = isset($_REQUEST['length']) ? $_REQUEST['length'] : '';
+		$search = isset($_REQUEST['search']['value']) ? $_REQUEST['search']['value'] : '';
+		$data = $this->Subcriteria->datatable($search, $start, $length);
+		$total_count = $this->Subcriteria->datatable($search);
+
+		echo json_encode([
+			'draw' => intval($param['draw']),
+			'recordsTotal' => count($total_count),
+			'recordsFiltered' => count($total_count),
+			'data' => $data
+		]);
 	}
 
+	/**
+	 * create data
+	 * @return void
+	 */
 	public function store()
 	{
+		if (!$this->input->is_ajax_request()) {
+			exit('No direct script access allowed');
+		}
+
 		$this->_rules();
 		if ($this->form_validation->run() == FALSE) {
-			$this->session->set_flashdata('message', '<div class="alert 
-			alert-danger" role="alert">Form input error</div>');
-			$this->create();
-		} else {
-			$data = array(
-				'criteria_id' => $this->input->post('criteria_id'),
-				'name' => $this->input->post('name'),
-				'weight' => $this->input->post('weight'),
-			);
+			$this->output->set_status_header(400);
+			$errors = $this->form_validation->error_array();
 
-			$this->Subcriteria->insert_data($data, 'subkriteria');
-			$this->session->set_flashdata('message', '<div class="alert 
-			alert-success" role="alert">Your data has been added!</div>');
-			redirect(base_url('subkriteria'));
+			$errorObj = [];
+			foreach ($errors as $key => $value) {
+				$errorObj[$key] = [[$value]];
+			}
+
+			echo json_encode(array('errors' => $errorObj));
+			return;
 		}
+
+		$data = array(
+			'criteria_id' => $this->input->post('criteria_id'),
+			'name' => $this->input->post('name'),
+			'weight' => $this->input->post('weight'),
+		);
+
+		$this->output->set_status_header(200);
+		$this->Subcriteria->insert_data($data, 'subkriteria');
+		echo json_encode(array('status' => "OK", 'code' => 200, 'message' => "Data subkriteria berhasil ditambahkan"));
 	}
 
-	public function update($id)
+	/**
+	 * get data by id
+	 * @param $id
+	 * @return void
+	 */
+	public function edit($id)
 	{
+		if (!$this->input->is_ajax_request()) {
+			exit('No direct script access allowed');
+		}
+
+		$this->output->set_status_header(200);
+		echo json_encode(array('success' => true, 'code' => 200, 'data' => $this->Subcriteria->get_data_by_id($id)));
+	}
+
+	/**
+	 * update data
+	 * @return void
+	 */
+	public function update()
+	{
+		if (!$this->input->is_ajax_request()) {
+			exit('No direct script access allowed');
+		}
+
 		$this->_rules();
 
 		if ($this->form_validation->run() == FALSE) {
-			$this->session->set_flashdata('message', '<div class="alert 
-			alert-danger" role="alert">Form input error</div>');
-			$this->index();
-		} else {
-			$data = array(
-				'id' => $id,
-				'criteria_id' => $this->input->post('criteria_id'),
-				'name' => $this->input->post('name'),
-				'weight' => $this->input->post('weight'),
-			);
-			$this->Subcriteria->update_data($data, 'subkriteria');
-			$this->session->set_flashdata('message', '<div class="alert 
-			alert-primary" role="alert">Your data has been updated!</div>');
-			redirect(base_url('subkriteria'));
+			$this->output->set_status_header(400);
+			$errors = $this->form_validation->error_array();
+
+			$errorObj = [];
+			foreach ($errors as $key => $value) {
+				$errorObj[$key] = [[$value]];
+			}
+
+			echo json_encode(array('errors' => $errorObj));
 		}
+
+		$data = array(
+			'id' => $this->input->post('id'),
+			'criteria_id' => strtoupper($this->input->post('criteria_id')),
+			'name' => $this->input->post('name'),
+			'weight' => $this->input->post('weight'),
+		);
+		$this->output->set_status_header(200);
+		$this->Subcriteria->update_data($data, 'subkriteria');
+		echo json_encode(array('status' => "OK", 'code' => 200, 'message' => "Data subkriteria berhasil diupdate"));
 	}
 
-	public function delete($id)
+	/**
+	 * delete data
+	 * @return void
+	 */
+	public function delete()
 	{
-		$where = array('id' => $id);
+		if (!$this->input->is_ajax_request()) {
+			exit('No direct script access allowed');
+		}
+
+		$where = array('id' => $this->input->post('id'));
 
 		$this->Subcriteria->delete($where, 'subkriteria');
-		$this->session->set_flashdata('message', '<div class="alert 
-			alert-warning" role="alert">Your data has been deleted!</div>');
-		redirect(base_url('subkriteria'));
+		$this->output->set_status_header(200);
+		echo json_encode(array('success' => true, 'code' => 200, 'message' => "Data subkriteria berhasil dihapus"));
 	}
 
+	/**
+	 * create rules validation
+	 * @return void
+	 */
 	public function _rules()
 	{
 		$this->form_validation->set_rules('criteria_id', 'Kriteria', 'required', array(
-			'required' => '%s field is required'
+			'required' => '%s field tidak boleh kosong'
 		));
 		$this->form_validation->set_rules('name', 'Nama', 'required', array(
-			'required' => '%s field is required',
+			'required' => '%s field tidak boleh kosong',
 		));
 		$this->form_validation->set_rules('weight', 'Bobot', 'required|numeric', array(
-			'required' => 'The %s field is required.',
-			'numeric' => 'The %s field must be numeric.',
+			'required' => 'The %s field tidak boleh kosong.',
+			'numeric' => 'The %s field harus berupa angka',
 		));
 	}
 }
